f48553431cfec0dbd92020d086978778bb59dcc1
TG49 <tomjgaertner@outlook.com>
2022-09-19 17:30:59 +0930

    Finished altitude and simtime graphs

--- Lander/Lander/Visualise Telemetry.py
+++ Lander/Lander/Visualise Telemetry.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
-altitude = np.loadtxt("Telemetry/altitude.txt")
-#desiredVelocity = np.loadtxt("Telemetry/targetVel.txt")
-#velocity = np.loadtxt("Telemetry/velocity.txt")
+#altitude = np.loadtxt("Telemetry/altitude.txt")
+desiredVelocity = np.loadtxt("Telemetry/targetVel.txt")
+velocity = np.loadtxt("Telemetry/velocity.txt")
 simulationtime = np.loadtxt("Telemetry/simtime.txt")
 
 #fig, ax = plt.subplots()
@@ -12,14 +12,16 @@ simulationtime = np.loadtxt("Telemetry/simtime.txt")
 #ax.plot(altitude, velocity, color = "blue")
 
 plt.figure(figsize=(20,15))
-plt.plot(simulationtime, altitude, color = "red")
+plt.plot(simulationtime, desiredVelocity, color = "blue", label = "Target Velocity")
+plt.plot(simulationtime, velocity, color = "red", label = "Velocity")
 
+plt.legend()
 ax = plt.gca()
-ax.set_ylabel("Altitude (m)")
+ax.set_ylabel("Velocity (m/s)")
 ax.set_xlabel("Simulation Time (s)")
 ax.set_title(' '.join(sys.argv[1:]))
 
-plt.savefig('../../Assignment 5/Altitude vs simulation time/' + ' '.join(sys.argv[1:])+'.png')
+plt.savefig('../../Assignment 5/Velocity vs time/' + ' '.join(sys.argv[1:])+'.png')
 plt.show()
 
 
